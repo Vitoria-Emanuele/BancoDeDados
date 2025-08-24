d2484221fe4125bdca6410b59b6a37f051cccfb0
Vitoria-Emanuele <vitoriaemanueledesousa@gmail.com>
2025-08-24 17:20:43 -0300

    chore: registrando as rotas na API e colocando no ar.

--- backend/App/main.py
+++ backend/App/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from .routers import rdistribuidor, rfornecedor, rfuncionario, ritemlote, ritemretirada, rlivro, rlote, rregistroentrada, rretirada
+
+
+app = FastAPI(title="API da Livraria", version="1.0")
 
 
 @app.get("/")
 def home():
-    return "minha api esta no ar"
+    return {"message": "minha api esta no ar "}
+
+
+# registra cada router
+app.include_router(rdistribuidor.router)
+app.include_router(rfornecedor.router)
+app.include_router(rfuncionario.router)
+app.include_router(rregistroentrada.router)
+app.include_router(rlote.router)
+app.include_router(rlivro.router)
+app.include_router(ritemlote.router)
+app.include_router(rretirada.router)
+app.include_router(ritemretirada.router)
